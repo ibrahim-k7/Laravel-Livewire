f098579feca15bf0ee3388be094b6b82465d1f95
ibrahim Mohammed <kxibrahimxk@gmail.com>
2025-01-20 22:35:26 +0300

    Role

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'key', 'color'];
+
+    protected $dates = ['deleted_at'];
+
+    public function permission()
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+
+    public function user()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function hasPermission($key)
+    {
+        return $this->permission->contains('key', $key);
+    }
+
+    // الاعضاء المحذوفين من هذا Role
+    public function userTrashed()
+    {
+        return $this->hasMany(User::class)->OnlyTrashed();
+    }
 }
