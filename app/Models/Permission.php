42b90916ad6015a6b51e9620f3423f6771993c9f
ibrahim Mohammed <kxibrahimxk@gmail.com>
2025-01-20 22:25:54 +0300

    Perission

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Permission extends Model
 {
-    use HasFactory;
+    use HasFactory ,SoftDeletes;
+
+    protected $fillable = ['name','key','table_name'];
+
+    protected $dates = ['deleted_at'];
+
+    public function role()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
