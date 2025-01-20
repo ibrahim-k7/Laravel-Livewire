b08cd7236b99f66adf022f21c62490bd2e546be5
ibrahim Mohammed <kxibrahimxk@gmail.com>
2025-01-20 23:42:07 +0300

    seeders

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Country;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +11,42 @@ class CountrySeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        Country::firstOrCreate([
+            'name' => 'Saudi Arabia',
+            'slug' => 'SA',
+            'iso3' => 'SAU'
+        ]);
+
+        Country::firstOrCreate([
+            'name' => 'United Arab Emirates',
+            'slug' => 'AE',
+            'iso3' => 'ARE'
+        ]);
+
+        Country::firstOrCreate([
+            'name' => 'Kuwait',
+            'slug' => 'KW',
+            'iso3' => 'KWT'
+        ]);
+
+        Country::firstOrCreate([
+            'name' => 'Qatar',
+            'slug' => 'QA',
+            'iso3' => 'QAT'
+        ]);
+
+        Country::firstOrCreate([
+            'name' => 'Bahrain',
+            'slug' => 'BH',
+            'iso3' => 'BHR'
+        ]);
+
+        Country::firstOrCreate([
+            'name' => 'Oman',
+            'slug' => 'OM',
+            'iso3' => 'OMN'
+        ]);
     }
 }
--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Permission;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +11,30 @@ class PermissionSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        Permission::firstOrCreate([
+            'name'      => 'Browse admin',
+            'key'       => 'browse_admin',
+            'table_name' => null,
+        ]);
+
+        Permission::firstOrCreate([
+            'name'      => 'Administrator',
+            'key'       => 'administrator',
+            'table_name' => null,
+        ]);
+
+        Permission::firstOrCreate([
+            'name'      => 'Banned',
+            'key'       => 'banned',
+            'table_name' => null,
+        ]);
+
+        Permission::generateFor('permissions');
+        Permission::generateFor('roles');
+        Permission::generateFor('countries');
+        Permission::generateFor('cities');
+        Permission::generateFor('users');
     }
 }
